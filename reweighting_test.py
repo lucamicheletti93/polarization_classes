903c6115d1f29335c84b67b33ead01b804a10e5b
lmichele <luca.micheletti@cern.ch>
2018-10-04 17:05:31 +0200

    Modification of reweighting_test.py

--- reweighting_test.py
+++ reweighting_test.py
@@ -1,28 +1,38 @@
 from ROOT import *
 from array import array
+import os.path
 
 gStyle.SetOptStat(0)
 gStyle.SetPaintTextFormat("0.2g");
 gROOT.ProcessLineSync(".x AccxEffCalculator.cxx+")
 gROOT.ProcessLineSync(".x Binning.cxx+")
 
-fileBinning = TFile.Open("/home/luca/GITHUB/polarization_classes/output/binning.root")
+fileBinning = TFile.Open("output/binning.root")
 binning = fileBinning.Get("Binning")
 CostValues = binning.GetCostValues()
 PhiValues = binning.GetPhiValues()
 
-#fileDataMC = TFile.Open("/afs/cern.ch/user/l/lmichele/CERNBox/JPSI/JPSI_POLARIZATION/JIRA_TICKET/READ_MC/OUTPUT/MC_official_tree_Jpsi_PbPb_Nopol.root")  # lxplus
-fileDataMC = TFile.Open("/home/luca/cernbox/JPSI/JPSI_POLARIZATION/JIRA_TICKET/READ_MC/OUTPUT/MC_official_tree_Jpsi_PbPb_Nopol.root") # local
+if os.path.isfile("/afs/cern.ch/user/l/lmichele/CERNBox/JPSI/JPSI_POLARIZATION/JIRA_TICKET/READ_MC/OUTPUT/MC_official_tree_Jpsi_PbPb_Nopol.root"):
+    fileDataMC = TFile.Open("/afs/cern.ch/user/l/lmichele/CERNBox/JPSI/JPSI_POLARIZATION/JIRA_TICKET/READ_MC/OUTPUT/MC_official_tree_Jpsi_PbPb_Nopol.root")  # lxplus
+else:
+    fileDataMC = TFile.Open("/home/luca/cernbox/JPSI/JPSI_POLARIZATION/JIRA_TICKET/READ_MC/OUTPUT/MC_official_tree_Jpsi_PbPb_Nopol.root") # local
+
 treeDataMC = fileDataMC.Get("MCTree")
 
 minPt = [0,2,4,6,10]
 maxPt = [2,4,6,10,1000]
 
-# lambda theta = 0.8
+print "########################################################################"
+print "Setting lambda theta = 0.8"
+lambdaTheta1 = 0.8
 AccxEffReWeight1 = AccxEffCalculator(treeDataMC)
 AccxEffReWeight1.SetPtBins(5,array('d',[0.,2.,4.,6.,10.]),array('d',[2.,4.,6.,10.,1000.]))
 AccxEffReWeight1.SetBinning(CostValues,PhiValues)
-AccxEffReWeight1.ReWeightAccxEff(0.8,"TestStat","output/AccxEffReWeightedPol1.root")
+
+if os.path.isfile("output/AccxEffReWeightedPol1.root"):
+    pass
+else:
+    AccxEffReWeight1.ReWeightAccxEff(0.8,"FullStat","output/AccxEffReWeightedPol1.root")
 
 fileAccxEffReWeightedPol1 = TFile.Open("output/AccxEffReWeightedPol1.root")
 
@@ -30,9 +40,9 @@ histGenCostReWeightedPol1 = []
 histAccxEffCostReWeightedPol1 = []
 
 for i in range(5):
-    histGenCostReWeightedPol1.append(fileAccxEffReWeightedPol1.Get('histGenCostReWeighted' + str(0.8) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
+    histGenCostReWeightedPol1.append(fileAccxEffReWeightedPol1.Get('histGenCostReWeighted' + str(lambdaTheta1) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
     histGenCostReWeightedPol1[i].SetDirectory(0)
-    histAccxEffCostReWeightedPol1.append(fileAccxEffReWeightedPol1.Get('histAccxEffCostReWeighted' + str(0.8) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
+    histAccxEffCostReWeightedPol1.append(fileAccxEffReWeightedPol1.Get('histAccxEffCostReWeighted' + str(lambdaTheta1) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
     histAccxEffCostReWeightedPol1[i].SetDirectory(0)
 fileDataMC.Close()
 fileAccxEffReWeightedPol1.Close()
@@ -41,11 +51,17 @@ fileAccxEffReWeightedPol1.Close()
 fileDataMC = TFile.Open("/home/luca/cernbox/JPSI/JPSI_POLARIZATION/JIRA_TICKET/READ_MC/OUTPUT/MC_official_tree_Jpsi_PbPb_Nopol.root") # local
 treeDataMC = fileDataMC.Get("MCTree")
 
-# lambda theta = 0.0
+print "########################################################################"
+print "Setting lambda theta = 0.0"
+lambdaTheta2 = 0.0
 AccxEffReWeight2 = AccxEffCalculator(treeDataMC)
 AccxEffReWeight2.SetPtBins(5,array('d',[0.,2.,4.,6.,10.]),array('d',[2.,4.,6.,10.,1000.]))
 AccxEffReWeight2.SetBinning(CostValues,PhiValues)
-AccxEffReWeight2.ReWeightAccxEff(0.0,"TestStat","output/AccxEffReWeightedPol2.root")
+
+if os.path.isfile("output/AccxEffReWeightedPol2.root"):
+    pass
+else:
+    AccxEffReWeight2.ReWeightAccxEff(0.0,"FullStat","output/AccxEffReWeightedPol2.root")
 
 fileAccxEffReWeightedPol2 = TFile.Open("output/AccxEffReWeightedPol2.root")
 
@@ -53,10 +69,10 @@ histGenCostReWeightedPol2 = []
 histAccxEffCostReWeightedPol2 = []
 
 for i in range(5):
-    histGenCostReWeightedPol2.append(fileAccxEffReWeightedPol2.Get('histGenCostReWeighted' + str(0.0) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
+    histGenCostReWeightedPol2.append(fileAccxEffReWeightedPol2.Get('histGenCostReWeighted' + str(lambdaTheta2) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
     histGenCostReWeightedPol2[i].SetDirectory(0)
     histGenCostReWeightedPol2[i].SetLineColor(kRed)
-    histAccxEffCostReWeightedPol2.append(fileAccxEffReWeightedPol2.Get('histAccxEffCostReWeighted' + str(0.0) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
+    histAccxEffCostReWeightedPol2.append(fileAccxEffReWeightedPol2.Get('histAccxEffCostReWeighted' + str(lambdaTheta2) + '_' + str(minPt[i]) + 'pT' + str(maxPt[i])))
     histAccxEffCostReWeightedPol2[i].SetDirectory(0)
     histAccxEffCostReWeightedPol2[i].SetLineColor(kRed)
 fileDataMC.Close()
--- test_DataProcessor.py
+++ test_DataProcessor.py
@@ -8,20 +8,23 @@ gROOT.ProcessLineSync(".x DataProcessor.cxx+")
 
 if os.path.isfile("/media/luca/488AE2208AE20A70/PbPb_2015_Trees_pDCA/new_tree/Tree_full_stat.root"):
     fileData = TFile.Open("/media/luca/488AE2208AE20A70/PbPb_2015_Trees_pDCA/new_tree/Tree_full_stat.root")  # local
+    treeData = fileData.Get("PbPbTree")
+    test = DataProcessor(treeData)
+    test.ComputeTriggerResponseFunction("FullStat","output/TriggerRespondeFunctionData.root")
+
 else:
-    print "This class can run only in local on the hard disk!!!"
+    print "This class can be run only in local on the hard disk!!!"
 
-treeData = fileData.Get("PbPbTree")
+if os.path.isfile("output/TriggerRespondeFunctionData.root"):
+    fileTriggerResponseFunctionData = TFile.Open("output/TriggerRespondeFunctionData.root")
+    histTriggerResponseFunctionSM = fileTriggerResponseFunctionData.Get("histTriggerResponseFunctionSM")
+    histTriggerResponseFunctionSMpDCA = fileTriggerResponseFunctionData.Get("histTriggerResponseFunctionSMpDCA")
 
-test = DataProcessor(treeData)
-#test.ComputeTriggerResponseFunction("FullStat","output/TriggerRespondeFunctionData.root")
+    canvasTriggerResponseFunctionComp = TCanvas("canvasTriggerResponseFunctionComp","canvasTriggerResponseFunctionComp",20,20,600,600)
+    histTriggerResponseFunctionSM.Draw()
+    histTriggerResponseFunctionSMpDCA.Draw("same")
 
-fileTriggerResponseFunctionData = TFile.Open("output/TriggerRespondeFunctionData.root")
-histTriggerResponseFunctionSM = fileTriggerResponseFunctionData.Get("histTriggerResponseFunctionSM")
-histTriggerResponseFunctionSMpDCA = fileTriggerResponseFunctionData.Get("histTriggerResponseFunctionSMpDCA")
-
-canvasTriggerResponseFunctionComp = TCanvas("canvasTriggerResponseFunctionComp","canvasTriggerResponseFunctionComp",20,20,600,600)
-histTriggerResponseFunctionSM.Draw()
-histTriggerResponseFunctionSMpDCA.Draw("same")
+else:
+    print "Connect the hard disk to produce the file output/TriggerRespondeFunctionData.root!!!"
 
 raw_input()
